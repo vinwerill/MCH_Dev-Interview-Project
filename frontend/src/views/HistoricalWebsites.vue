8fa1a928df13f49156bac0f4321674f36a889abb
vinwerill <vincent.super8@gmail.com>
2025-03-04 23:51:43 +0800

    Adjust export funection

--- frontend/src/views/HistoricalWebsites.vue
+++ frontend/src/views/HistoricalWebsites.vue
@@ -25,17 +25,16 @@
 // TODO: Fetch websites data from API
 import axios from 'axios'
 
+var data;
+axios.get('https://mch-dev.userwei.com/api/websites')
+      .then(response => {
+        data = response.data;
+      })
 export default {
   data() {
     return {
-      websites: []
+      websites: data
     };
-  },
-  mounted() {
-    axios.get('https://mch-dev.userwei.com/api/websites')
-      .then(response => {
-        this.websites = response.data;
-      })
   }
 };
 </script>
